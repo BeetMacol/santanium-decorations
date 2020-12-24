823877c313ece76e13e3d524e1e9fae531f74e91
BeetMacol <beetmacol@gmail.com>
2020-12-24 15:20:03 +0100

    Santanium ore is now made out of snow!

--- src/main/java/com/beetmacol/santaniumdecorations/SantaniumDecorations.java
+++ src/main/java/com/beetmacol/santaniumdecorations/SantaniumDecorations.java
@@ -1,15 +1,6 @@
 package com.beetmacol.santaniumdecorations;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
